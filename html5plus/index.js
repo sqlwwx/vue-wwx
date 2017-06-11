9b681fbee39b1cbda3c1f279f46e5a61ac52fe89
sqlwwx <wwx_2012@hotmail.com>
2017-06-11 08:52:53 +0800

    Vue.prototype.$closeWaiting

--- html5plus/index.js
+++ html5plus/index.js
@@ -15,13 +15,20 @@ export default {
       return this.$plus.then(plus => {
         return Promise.resolve(plus.nativeUI.toast(title, options))
       }, () => {
-        window.alert('todo:web toast ' + title)
+        window.console.warn('todo:web toast ' + title)
       })
     }
+    let waitings = {}
     Vue.prototype.$showWaiting = function (title, options = {}) {
       return this.$plus.then(plus => {
-        return Promise.resolve(plus.nativeUI.showWaiting(title, options))
+        const id = options.id || title
+        waitings[id] = plus.nativeUI.showWaiting(title, options)
+        return Promise.resolve(waitings[id])
       })
+    }
+    Vue.prototype.$closeWaiting = function (id) {
+      waitings[id] && waitings[id].close()
+      delete waitings[id]
     }
   }
 }
