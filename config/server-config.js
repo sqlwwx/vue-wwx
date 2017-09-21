6b2269e9050b066e1841d3a1f10fb2d7c3b77bde
sqlwwx <wwx_2012@hotmail.com>
2017-09-21 20:54:49 +0800

    lost import

--- config/server-config.js
+++ config/server-config.js
@@ -1,3 +1,6 @@
+import Vue from 'vue'
+import lstore from 'store'
+
 export const serverConfig = (code) => {
   return lstore.getWithExpire('config:' + code, () => {
     return Vue.http.get('Configs/findByCode/' + code).then((res) => {
